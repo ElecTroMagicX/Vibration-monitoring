d23e00fd0f5954d36bb57b125cb8560b14671856
DY X <934112474@qq.com>
2020-06-30 09:48:39 +0800

    https://github.com/Deeplfree/Vibration-monitoring

--- 震动监测系统/controlconfig.cs
+++ 震动监测系统/controlconfig.cs
@@ -1,3 +1,8 @@
+
+/*****************************
+ * https://github.com/Deeplfree/Vibration-monitoring
+ *****************************/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
